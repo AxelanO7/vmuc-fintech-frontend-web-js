9ebc20d3dd9ac616fe4c470d541b9b66b5b45e0b
AxelanO7 <jeremia123.jm@gmail.com>
2024-11-24 16:15:26 +0800

    add general journal page and modal for manipulating reference posts

---
+++ src/pages/journal/general/general_journal_page.tsx
@@ -0,0 +1,133 @@
+import {
+  HomeIcon,
+  MagnifyingGlassIcon,
+  TrashIcon,
+} from "@heroicons/react/16/solid";
+import {
+  Button,
+  Input,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+  useDisclosure,
+} from "@nextui-org/react";
+import ManipulateRefPostDialogComponent from "./manipulate_general_journal_modal";
+import { useState } from "react";
+import DefaultLayout from "../../../layouts/default_layout";
+
+export interface RefPostData {
+  id: number;
+  accountName: string;
+  accountCode: string;
+  accountType: string;
+}
+
+export default function GeneralJournalPage() {
+  // Manipulate Modal
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [accountName, setAccountName] = useState<string>("");
+  const [accountCode, setAccountCode] = useState<string>("");
+  const [accountType, setAccountType] = useState<string>("");
+
+  const renderManipulateRefPostDialog = ({
+    action,
+    dataEdit,
+  }: {
+    action: string;
+    dataEdit?: RefPostData;
+  }) => {
+    return (
+      <ManipulateRefPostDialogComponent
+        isOpen={isOpen}
+        onOpen={onOpen}
+        onOpenChange={onOpenChange}
+        accountName={accountName}
+        setAccountName={setAccountName}
+        accountCode={accountCode}
+        setAccountCode={setAccountCode}
+        accountType={accountType}
+        setAccountType={setAccountType}
+        dataEdit={dataEdit}
+        action={action}
+        onSave={() => handleOnSaveManipulateRefPost({ action })}
+      />
+    );
+  };
+
+  const handleOnSaveManipulateRefPost = ({ action }: { action: string }) => {
+    console.log("Account Name: ", accountName);
+    console.log("Account Code: ", accountCode);
+    console.log("Account Type: ", accountType);
+
+    if (action === "add") {
+      console.log("Add Ref Post");
+    } else {
+      console.log("Edit Ref Post");
+    }
+  };
+  // End Manipulate Modal
+
+  const refPostData = [
+    {
+      id: 1,
+      accountName: "Kas",
+      accountCode: "110",
+      accountType: "Asset",
+    },
+  ];
+
+  return (
+    <DefaultLayout>
+      <h1 className="text-3xl font-bold mx-6 pt-4">Akun Ref Post</h1>
+      <div className="flex items-center bg-gray-300 px-6 py-2">
+        <HomeIcon className="w-5 h-5" />
+        <p className="ml-2 font-semibold">Akun Ref Post</p>
+      </div>
+      <div className="bg-gray-200 m-4 p-8">
+        <h1 className="text-3xl font-medium text-gray-600">Akun Ref Post</h1>
+        <div className="flex justify-between mt-4">
+          {renderManipulateRefPostDialog({ action: "add" })}
+          <div className="flex gap-2">
+            <Input placeholder="Cari" />
+            <Button color="primary" isIconOnly>
+              <MagnifyingGlassIcon className="w-5 h-5" />
+            </Button>
+          </div>
+        </div>
+        <Table aria-label="Periode Table" className="mt-8">
+          <TableHeader>
+            <TableColumn className="text-center">Nama Akun</TableColumn>
+            <TableColumn className="text-center">Kode Akun</TableColumn>
+            <TableColumn className="text-center">Tipe Akun</TableColumn>
+            <TableColumn className="w-40 text-center">Aksi</TableColumn>
+          </TableHeader>
+          <TableBody>
+            {refPostData.map((data) => (
+              <TableRow key={data.id} className="bg-gray-50">
+                <TableCell className="text-center">
+                  {data.accountName}
+                </TableCell>
+                <TableCell className="text-center">
+                  {data.accountCode}
+                </TableCell>
+                <TableCell className="text-center">
+                  {data.accountType}
+                </TableCell>
+                <TableCell className="text-center flex justify-evenly">
+                  {renderManipulateRefPostDialog({
+                    action: "edit",
+                    dataEdit: data,
+                  })}
+                  <TrashIcon className="text-danger w-10 h-10" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </DefaultLayout>
+  );
+}
---
+++ src/pages/journal/general/manipulate_general_journal_modal.tsx
@@ -0,0 +1,159 @@
+import { PencilIcon, PlusIcon } from "@heroicons/react/16/solid";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  Input,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+} from "@nextui-org/react";
+import { RefPostData } from "./general_journal_page";
+
+export default function ManipulateGeneralJournalModal({
+  isOpen,
+  onOpen,
+  onOpenChange,
+  accountName,
+  setAccountName,
+  accountCode,
+  setAccountCode,
+  accountType,
+  setAccountType,
+  dataEdit,
+  action,
+  onSave,
+}: {
+  isOpen: boolean;
+  onOpen: () => void;
+  onOpenChange: (open: boolean) => void;
+  accountName: string;
+  setAccountName: (name: string) => void;
+  accountCode: string;
+  setAccountCode: (code: string) => void;
+  accountType: string;
+  setAccountType: (type: string) => void;
+  dataEdit?: RefPostData;
+  action: string;
+  onSave: ({ action }: { action: string }) => void;
+}) {
+  const typeAccount = [
+    {
+      key: 1,
+      label: "Aktiva",
+    },
+    {
+      key: 2,
+      label: "Kewajiban",
+    },
+    {
+      key: 3,
+      label: "Modal",
+    },
+    {
+      key: 4,
+      label: "Pendapatan",
+    },
+    {
+      key: 5,
+      label: "Beban",
+    },
+  ];
+
+  const isEdit = action === "edit";
+
+  return (
+    <>
+      {isEdit ? (
+        <PencilIcon
+          className="text-secondary w-10 h-10"
+          onClick={() => {
+            setAccountName(dataEdit?.accountName || "");
+            setAccountCode(dataEdit?.accountCode || "");
+            setAccountType(dataEdit?.accountType || "");
+            onOpen();
+          }}
+        />
+      ) : (
+        <Button className="w-max" color="primary" onPress={onOpen}>
+          <PlusIcon className="w-5 h-5" />
+          Tambah Akun
+        </Button>
+      )}
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                {isEdit ? "Edit" : "Tambah"}
+                Ref Post
+              </ModalHeader>
+              <ModalBody>
+                <Input
+                  autoFocus
+                  label="Nama Akun"
+                  placeholder="Masukkan nama akun"
+                  variant="bordered"
+                  defaultValue={isEdit ? accountName : ""}
+                  onChange={(e) => setAccountName(e.target.value)}
+                />
+                <Dropdown>
+                  <DropdownTrigger>
+                    <div className="">
+                      <Input
+                        label="Tipe Akun"
+                        placeholder="Pilih tipe akun"
+                        variant="bordered"
+                        value={accountType}
+                        className="cursor-pointer"
+                        defaultValue={isEdit ? accountType : "Pilih tipe akun"}
+                        readOnly
+                      />
+                    </div>
+                  </DropdownTrigger>
+                  <DropdownMenu>
+                    {typeAccount.map((item, index) => (
+                      <DropdownItem
+                        key={index}
+                        onClick={() => {
+                          setAccountType(item.label);
+                        }}
+                      >
+                        {item.label}
+                      </DropdownItem>
+                    ))}
+                  </DropdownMenu>
+                </Dropdown>
+                <Input
+                  label="Kode Akun"
+                  placeholder="Masukkan kode akun"
+                  variant="bordered"
+                  defaultValue={isEdit ? accountCode : ""}
+                  onChange={(e) => setAccountCode(e.target.value)}
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="flat" onPress={onClose}>
+                  Batal
+                </Button>
+                <Button
+                  color="primary"
+                  onPress={() => {
+                    onSave({ action });
+                    onClose();
+                  }}
+                >
+                  Simpan
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
