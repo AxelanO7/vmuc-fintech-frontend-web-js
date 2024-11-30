f68336c101bddcde83965082dc081390f53d67a8
AxelanO7 <jeremia123.jm@gmail.com>
2024-11-30 16:21:11 +0800

    refactor user profile component for enhanced performance; streamline data fetching and improve loading times

--- src/pages/journal/general/add_general_journal_page.tsx
+++ src/pages/journal/general/add_general_journal_page.tsx
@@ -1,5 +1,9 @@
-import { TrashIcon } from "@heroicons/react/16/solid";
 import {
+  Dropdown,
+  DropdownMenu,
+  DropdownSection,
+  DropdownTrigger,
+  Input,
   Table,
   TableBody,
   TableCell,
@@ -8,88 +12,63 @@ import {
   TableRow,
 } from "@nextui-org/react";
 import DefaultLayout from "../../../layouts/default_layout";
-import { generalJournalType } from "../../../core/interfaces/data";
 import { breadcrumsItem } from "../../../core/interfaces/props";
 import Breadcrumb from "../../../components/breadcrumb";
+import { useState } from "react";
+import {
+  generalJournalContentType,
+  refPostType,
+} from "../../../core/interfaces/data";
+import { ChevronDownIcon, TrashIcon } from "@heroicons/react/16/solid";
 
 export default function AddGeneralJournalPage() {
+  const [, setPeriode] = useState("");
+  const [, setDescription] = useState("");
+
   // ~*~ // Table // ~*~ //
-  const tableItems: generalJournalType[] = [
+  const [tableItems, setTableItems] = useState<generalJournalContentType[]>([
     {
+      credit: 0,
+      debit: 0,
+      information: "",
       id: 1,
-      period: "Mei/2024",
-      description: "Jurnal Umum",
-      contents: [
-        {
-          id: 1,
-          date: "01/05/2024",
-          ref_post: "1",
-          information: "Pembelian Barang",
-          debit: 1000000,
-          credit: 0,
-        },
-        {
-          id: 2,
-          date: "01/05/2024",
-          ref_post: "2",
-          information: "Pembelian Barang",
-          debit: 0,
-          credit: 1000000,
-        },
-      ],
+      date: "01-01-2021",
+      ref_post: {
+        accountCode: "001",
+        accountName: "test",
+        accountType: "test",
+        id: 1,
+      },
+    },
+  ]);
+
+  const refPostItems: refPostType[] = [
+    {
+      accountCode: "001",
+      accountName: "test",
+      accountType: "test",
+      id: 1,
     },
     {
+      accountCode: "002",
+      accountName: "test",
+      accountType: "test",
       id: 2,
-      period: "Mei/2024",
-      description: "Jurnal Umum",
-      contents: [
-        {
-          id: 1,
-          date: "01/05/2024",
-          ref_post: "1",
-          information: "Pembelian Barang",
-          debit: 1000000,
-          credit: 0,
-        },
-        {
-          id: 2,
-          date: "01/05/2024",
-          ref_post: "2",
-          information: "Pembelian Barang",
-          debit: 0,
-          credit: 1000000,
-        },
-      ],
     },
   ];
 
-  const tableHeaderParentItems = [
+  const tableHeaderItems = [
     {
-      name: "#",
-      className: "w-12",
-    },
-    {
-      name: "Periode",
+      name: "Nama Akun",
       className: "",
     },
-    {
-      name: "Deskripsi",
-      className: "",
-    },
-    {
-      name: "Aksi",
-      className: "w-40",
-    },
-  ];
-
-  const tableHeaderChildItems = [
     {
       name: "Tanggal",
       className: "",
     },
     {
       name: "Ref Post",
-      className: "",
+      className: "w-20",
     },
     {
       name: "Keterangan",
@@ -105,7 +84,7 @@ export default function AddGeneralJournalPage() {
     },
     {
       name: "Aksi",
-      className: "w-40",
+      className: "w-20",
     },
   ];
 
@@ -128,90 +107,174 @@ export default function AddGeneralJournalPage() {
   return (
     <DefaultLayout>
       <h1 className="text-3xl font-bold mx-6 pt-4">Jurnal Umum</h1>
+
       <Breadcrumb items={breadcrumbItems} />
+
       <div className="bg-gray-200 m-4 p-8">
         <h1 className="text-3xl font-medium text-gray-600">Jurnal Umum</h1>
-        <div className="mt-4">
-          {tableItems.map((item, index) => (
-            <div key={item.id}>
-              <Table aria-label="Periode Table">
-                <TableHeader>
-                  {tableHeaderParentItems.map((item) => (
-                    <TableColumn
-                      key={item.name}
-                      className={`text-center ${item.className}`}
-                    >
-                      {item.name}
-                    </TableColumn>
-                  ))}
-                </TableHeader>
-                <TableBody>
-                  <TableRow key={item.id} className="bg-gray-50">
-                    <TableCell className="text-center">{item.id}</TableCell>
-                    <TableCell className="text-center">{item.period}</TableCell>
-                    <TableCell className="text-center">
-                      {item.description}
-                    </TableCell>
-                    <TableCell className="text-center flex justify-evenly">
-                      {/* {renderManipulateComponent({
-                        action: "edit",
-                        dataEdit: item,
-                      })} */}
-                      <p>test</p>
-                      <TrashIcon className="text-danger w-6 h-6" />
-                    </TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-              <Table
-                aria-label="Periode Table"
-                className={`mt-2 ${
-                  index === tableItems.length - 1 ? "" : "mb-8"
-                }`}
-              >
-                <TableHeader>
-                  {tableHeaderChildItems.map((item) => (
-                    <TableColumn
-                      key={item.name}
-                      className={`text-center ${item.className}`}
-                    >
-                      {item.name}
-                    </TableColumn>
-                  ))}
-                </TableHeader>
-                <TableBody>
-                  {item.contents.map((content) => (
-                    <TableRow key={content.id} className="bg-gray-50">
-                      <TableCell className="text-center">
-                        {content.date}
-                      </TableCell>
-                      <TableCell className="text-center">
-                        {content.ref_post}
-                      </TableCell>
-                      <TableCell className="text-center">
-                        {content.information}
-                      </TableCell>
-                      <TableCell className="text-center">
-                        {content.debit}
-                      </TableCell>
-                      <TableCell className="text-center">
-                        {content.credit}
-                      </TableCell>
-                      <TableCell className="text-center flex justify-evenly">
-                        <p>Test</p>
-                        {/* {renderManipulateComponent({
-                          action: "edit",
-                          dataEdit: item,
-                        })} */}
-                        <TrashIcon className="text-danger w-6 h-6" />
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </div>
-          ))}
+
+        <div className="flex mt-8 gap-4 ml-8">
+          <div>
+            <p className="ml-2 font-normal">Periode</p>
+            <Input
+              type="date"
+              className="w-max"
+              onChange={(e) => setPeriode(e.target.value)}
+            />
+          </div>
+          <div>
+            <p className="ml-2 font-normal">Deskripsi</p>
+            <Input
+              className="w-96"
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
         </div>
+
+        <Table aria-label="Periode Table" className="mt-4">
+          <TableHeader>
+            {tableHeaderItems.map((item) => (
+              <TableColumn
+                key={item.name}
+                className={`text-center ${item.className}`}
+              >
+                {item.name}
+              </TableColumn>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {tableItems.map((item) => (
+              <TableRow key={item.id}>
+                <TableCell className="text-center">
+                  <Dropdown>
+                    <DropdownTrigger>
+                      <Input
+                        placeholder="Pilih nama akun"
+                        variant="bordered"
+                        value={item.ref_post.accountName}
+                        className="cursor-pointer"
+                        onChange={(e) =>
+                          setTableItems(
+                            tableItems.map((tableItem) =>
+                              tableItem.id === item.id
+                                ? {
+                                    ...tableItem,
+                                    ref_post: {
+                                      ...tableItem.ref_post,
+                                      accountName: e.target.value,
+                                    },
+                                  }
+                                : tableItem
+                            )
+                          )
+                        }
+                        readOnly
+                        endContent={<ChevronDownIcon className="w-5 h-5" />}
+                      />
+                    </DropdownTrigger>
+                    <DropdownMenu>
+                      {refPostItems.map((refPostItem) => (
+                        <DropdownSection key={refPostItem.id}>
+                          <Input
+                            label="Nama Akun"
+                            placeholder="Pilih nama akun"
+                            variant="bordered"
+                            value={refPostItem.accountName}
+                            className="cursor-pointer"
+                            readOnly
+                          />
+                        </DropdownSection>
+                      ))}
+                    </DropdownMenu>
+                  </Dropdown>
+                </TableCell>
+                <TableCell className="text-center">
+                  <Input
+                    type="date"
+                    placeholder="Tanggal"
+                    defaultValue={item.date}
+                    onChange={(e) =>
+                      setTableItems(
+                        tableItems.map((tableItem) =>
+                          tableItem.id === item.id
+                            ? {
+                                ...tableItem,
+                                date: e.target.value,
+                              }
+                            : tableItem
+                        )
+                      )
+                    }
+                  />
+                </TableCell>
+                <TableCell className="text-center">
+                  <Input
+                    placeholder="Ref Post"
+                    value={item.ref_post.accountCode}
+                    readOnly
+                  />
+                </TableCell>
+                <TableCell className="text-center">
+                  <Input
+                    placeholder="Keterangan"
+                    defaultValue={item.information}
+                    onChange={(e) =>
+                      setTableItems(
+                        tableItems.map((tableItem) =>
+                          tableItem.id === item.id
+                            ? {
+                                ...tableItem,
+                                information: e.target.value,
+                              }
+                            : tableItem
+                        )
+                      )
+                    }
+                  />
+                </TableCell>
+                <TableCell className="text-center">
+                  <Input
+                    placeholder="Debit"
+                    defaultValue={item.debit.toString()}
+                    onChange={(e) =>
+                      setTableItems(
+                        tableItems.map((tableItem) =>
+                          tableItem.id === item.id
+                            ? {
+                                ...tableItem,
+                                debit: parseInt(e.target.value),
+                              }
+                            : tableItem
+                        )
+                      )
+                    }
+                  />
+                </TableCell>
+                <TableCell className="text-center">
+                  <Input
+                    placeholder="Kredit"
+                    defaultValue={item.credit.toString()}
+                    onChange={(e) =>
+                      setTableItems(
+                        tableItems.map((tableItem) =>
+                          tableItem.id === item.id
+                            ? {
+                                ...tableItem,
+                                credit: parseInt(e.target.value),
+                              }
+                            : tableItem
+                        )
+                      )
+                    }
+                  />
+                </TableCell>
+                <TableCell className="text-center flex justify-evenly">
+                  <TrashIcon className="text-danger w-6 h-6" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     </DefaultLayout>
   );
