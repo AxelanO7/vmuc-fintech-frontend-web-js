45cafa672630df6db85c207c177a84972e14fe10
AxelanO7 <jeremia123.jm@gmail.com>
2024-11-30 16:52:56 +0800

    enhance AddGeneralJournalPage with new button for adding rows; improve layout and total calculations

--- src/pages/journal/general/add_general_journal_page.tsx
+++ src/pages/journal/general/add_general_journal_page.tsx
@@ -1,4 +1,5 @@
 import {
+  Button,
   Dropdown,
   DropdownMenu,
   DropdownSection,
@@ -19,7 +20,11 @@ import {
   generalJournalContentType,
   refPostType,
 } from "../../../core/interfaces/data";
-import { ChevronDownIcon, TrashIcon } from "@heroicons/react/16/solid";
+import {
+  ChevronDownIcon,
+  PlusIcon,
+  TrashIcon,
+} from "@heroicons/react/16/solid";
 
 export default function AddGeneralJournalPage() {
   const [, setPeriode] = useState("");
@@ -110,7 +115,7 @@ export default function AddGeneralJournalPage() {
 
       <Breadcrumb items={breadcrumbItems} />
 
-      <div className="bg-gray-200 m-4 p-8">
+      <div className="bg-gray-200 mt-4 mx-4 py-8 px-4">
         <h1 className="text-3xl font-medium text-gray-600">Jurnal Umum</h1>
 
         <div className="flex mt-8 gap-4 ml-8">
@@ -130,8 +135,10 @@ export default function AddGeneralJournalPage() {
             />
           </div>
         </div>
+      </div>
 
-        <Table aria-label="Periode Table" className="mt-4">
+      <div className="bg-gray-200 mx-4 mt-4 p-4">
+        <Table aria-label="Periode Table">
           <TableHeader>
             {tableHeaderItems.map((item) => (
               <TableColumn
@@ -269,12 +276,73 @@ export default function AddGeneralJournalPage() {
                   />
                 </TableCell>
                 <TableCell className="text-center flex justify-evenly">
-                  <TrashIcon className="text-danger w-6 h-6" />
+                  <TrashIcon
+                    className="text-danger w-6 h-6"
+                    onClick={() =>
+                      setTableItems(
+                        tableItems.filter(
+                          (tableItem) => tableItem.id !== item.id
+                        )
+                      )
+                    }
+                  />
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
+      </div>
+
+      <div className="bg-gray-200 mx-4 mt-4 p-4 flex justify-center">
+        <Button
+          className="w-1/2 text-white flex"
+          startContent={<PlusIcon className="w-5 h-5" />}
+          color="secondary"
+          onClick={() =>
+            setTableItems([
+              ...tableItems,
+              {
+                credit: 0,
+                debit: 0,
+                information: "",
+                id: tableItems.length + 1,
+                date: "01-01-2021",
+                ref_post: {
+                  accountCode: "001",
+                  accountName: "test",
+                  accountType: "test",
+                  id: 1,
+                },
+              },
+            ])
+          }
+        >
+          Tambah Baris Baru
+        </Button>
+      </div>
+
+      <div className="bg-gray-200 mx-4 mt-4 p-4">
+        <div className="flex w-1/2 pl-12 mt-4 font-medium">
+          <p className="w-1/3">Total</p>
+          <p className="w-1/3">
+            Rp. {tableItems.reduce((acc, item) => acc + item.debit, 0)}
+          </p>
+          <p className="w-1/3">
+            Rp. {tableItems.reduce((acc, item) => acc + item.credit, 0)}
+          </p>
+        </div>
+        <div className="flex w-1/2 mt-4 pl-12 font-medium">
+          <p className="w-1/3">Selisih</p>
+          <div className="w-1/3" />
+          <p className="w-1/3">
+            Rp.{" "}
+            {tableItems.reduce((acc, item) => acc + item.debit, 0) -
+              tableItems.reduce((acc, item) => acc + item.credit, 0)}
+          </p>
+        </div>
+        <div className="w-full flex justify-end mt-4">
+          <Button color="primary">Simpan</Button>
+        </div>
       </div>
     </DefaultLayout>
   );
