30ba8efc09ef8e39015c34d4353efeb8d442296e
AxelanO7 <jeremia123.jm@gmail.com>
2025-01-06 02:49:38 +0800

    refactor api helper functions to improve base URL management for reference posts and employees

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,3 +1,11 @@
-export const getBaseUrl = () => {
+export const baseUrl = () => {
   return "http://localhost:4000";
+};
+
+export const baseUrlRefPost = () => {
+  return `${baseUrl()}/ref/private/account`;
+};
+
+export const baseUrlEmployee = () => {
+  return `${baseUrl()}/employee/private/account`;
 };
