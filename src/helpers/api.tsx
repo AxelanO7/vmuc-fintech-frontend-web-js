70767894f631d22cafb2c776d788f9b4347bf4d4
AxelanO7 <jeremia123.jm@gmail.com>
2025-01-11 22:39:58 +0800

    refactor API helpers to replace base URL functions with axios-based methods for improved API interaction

---
+++ src/helpers/api.tsx
@@ -0,0 +1,83 @@
+import axios from "axios";
+import { baseUrl } from "./url";
+
+export const ApiHelpers = {
+  get: async ({
+    url,
+    params,
+    successCallback,
+    errorCallback,
+  }: {
+    url: string;
+    params?: any;
+    successCallback: (response: any) => void;
+    errorCallback: (error: any) => void;
+  }) => {
+    try {
+      const res = await axios.get(`${baseUrl()}${url}`, { params });
+      console.log("GET request success", res);
+      successCallback(res);
+    } catch (error) {
+      console.log("GET request failed", error);
+      errorCallback(error);
+    }
+  },
+  post: async ({
+    url,
+    data,
+    successCallback,
+    errorCallback,
+  }: {
+    url: string;
+    data?: any;
+    successCallback: (response: any) => void;
+    errorCallback: (error: any) => void;
+  }) => {
+    try {
+      const res = await axios.post(`${baseUrl()}${url}`, data);
+      console.log("POST request success", res);
+      successCallback(res);
+    } catch (error) {
+      console.log("POST request failed", error);
+      errorCallback(error);
+    }
+  },
+  put: async ({
+    url,
+    data,
+    successCallback,
+    errorCallback,
+  }: {
+    url: string;
+    data?: any;
+    successCallback: (response: any) => void;
+    errorCallback: (error: any) => void;
+  }) => {
+    try {
+      const res = await axios.put(`${baseUrl()}${url}`, data);
+      console.log("PUT request success", res);
+      successCallback(res);
+    } catch (error) {
+      console.log("PUT request failed", error);
+      errorCallback(error);
+    }
+  },
+  delete: async ({
+    url,
+    successCallback,
+    errorCallback,
+  }: {
+    url: string;
+    successCallback: (response: any) => void;
+    errorCallback: (error: any) => void;
+  }) => {
+    try {
+      const res = await axios.delete(`${baseUrl()}${url}`);
+      console.log("DELETE request success", res);
+      successCallback(res);
+    } catch (error) {
+      console.log("DELETE request failed", error);
+      errorCallback(error);
+    }
+  },
+};
