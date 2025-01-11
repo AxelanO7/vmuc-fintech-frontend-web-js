d32e0893f9bfd08a1efdcccee788697f4c6301d4
AxelanO7 <jeremia123.jm@gmail.com>
2025-01-11 22:40:14 +0800

    add base URL helper functions for API endpoints

---
+++ src/helpers/url.ts
@@ -0,0 +1,11 @@
+export const baseUrl = () => {
+  return "http://localhost:4000";
+};
+
+export const baseUrlRefPost = () => {
+  return `/ref/private/post`;
+};
+
+export const baseUrlEmployeeAccount = () => {
+  return `/employee/private/account`;
+};
