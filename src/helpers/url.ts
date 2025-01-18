a76397c2ac72769722d68585c11b5f4f9c329139
AxelanO7 <jeremia123.jm@gmail.com>
2025-01-18 23:45:28 +0800

    refactor URL helpers to replace payroll terminology with period terminology for consistency in API endpoints

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -2,6 +2,13 @@ export const Urls = {
   base: "http://localhost:4000",
   refPost: "/ref/private/post",
   employeeAccount: "/employee/private/account",
-  payrollEmployee: "/payroll/private/employee",
+  // payrollEmployee: "/payroll/private/employee",
   payrollPeriodEmployee: "/payroll-periode/private/employee",
+  // * period
+  periodPayrollEmployee: "periode/private/payroll-employee",
+  periodAdjusmentEntries: "periode/private/adjusment-entries",
+  periodGeneralJournal: "periode/private/general-journal",
+  periodTrialBalance: "periode/private/trial-balance",
+  periodEmployee: "periode/private/employee",
+  periodEmployees: "periode/private/employees",
 };
