edd8ef2281c031c59f257fb7db4e7338c9ca6847
AxelanO7 <jeremia123.jm@gmail.com>
2025-01-20 01:52:00 +0800

    refactor URL helpers to rename payrollPeriodEmployee to generalLedgerEmployee and add periodGeneral endpoint for improved clarity

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -2,12 +2,13 @@ export const Urls = {
   base: "http://localhost:4000",
   refPost: "/ref/private/post",
   employeeAccount: "/employee/private/account",
-  payrollPeriodEmployee: "/payroll-periode/private/employee",
+  generalLedgerEmployee: "/general-ledger/private/employee",
+
   periodPayrollEmployee: "/periode/private/payroll-employee",
   periodAdjusmentEntries: "/periode/private/adjusment-entries",
   periodGeneralJournal: "/periode/private/general-journal",
   periodTrialBalance: "/periode/private/trial-balance",
-  periodEmployee: "/periode/private/employee",
-  periodEmployees: "/periode/private/employees",
+
+  periodGeneral: "/periode/private/general",
   periodPayroll: "/periode/private/payroll",
 };
