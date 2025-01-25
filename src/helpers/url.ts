2e3483bc8ffb61ed104e57e6e21d2f5b1a76640c
AxelanO7 <jeremia123.jm@gmail.com>
2025-01-25 23:13:02 +0800

    feat(urls): add journalLedger endpoint and improve URL structure

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -2,9 +2,10 @@ export const Urls = {
   base: "http://localhost:4000",
   refPost: "/ref/private/post",
   employeeAccount: "/employee/private/account",
-  ledgerEmployee: "/general-ledger/private/employee",
   payrollEmployee: "/payroll/private/employee",
+
   journalGeneral: "/journal/private/general",
+  journalLedger: "/journal/private/ledger",
 
   periodGeneral: "/periode/private/general",
   periodPayrollEmployee: "/periode/private/payroll-employee",
