c1aea5c52f0377e947a1ced8c080c9b8d98520bc
AxelanO7 <jeremia123.jm@gmail.com>
2025-02-06 20:51:50 +0800

    feat(AdjustmentEntryPage): refactor table rendering and enhance structure for clarity

--- src/pages/journal/adjustment-entry/adjustment_entry_page.tsx
+++ src/pages/journal/adjustment-entry/adjustment_entry_page.tsx
@@ -142,7 +142,7 @@ export default function AdjustmentEntryPage() {
         </div>
 
         <div className="mt-4">
-          {tableItems &&
+          {/* {tableItems &&
             tableItems.map((item, index) => (
               <div key={item.id}>
                 <Table aria-label="Periode Table">
@@ -167,7 +167,6 @@ export default function AdjustmentEntryPage() {
                       </TableCell>
                       <TableCell className="text-center flex justify-evenly">
                         <DocumentArrowDownIcon className="text-primary w-6 h-6" />
-                        <TrashIcon className="text-danger w-6 h-6" />
                       </TableCell>
                     </TableRow>
                   </TableBody>
@@ -217,7 +216,35 @@ export default function AdjustmentEntryPage() {
                   </TableBody>
                 </Table>
               </div>
-            ))}
+            ))} */}
+
+          <Table aria-label="Periode Table">
+            <TableHeader>
+              {tableHeaderParentItems.map((item) => (
+                <TableColumn
+                  key={item.name}
+                  className={`text-center ${item.className}`}
+                >
+                  {item.name}
+                </TableColumn>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {tableItems &&
+                tableItems.map((item, index) => (
+                  <TableRow key={item.id} className="bg-gray-50">
+                    <TableCell className="text-center">{index + 1}</TableCell>
+                    <TableCell className="text-center">{item.period}</TableCell>
+                    <TableCell className="text-center">
+                      {item.description}
+                    </TableCell>
+                    <TableCell className="text-center flex justify-evenly">
+                      <DocumentArrowDownIcon className="text-primary w-6 h-6" />
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
         </div>
       </div>
     </DefaultLayout>
